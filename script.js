324893ce3a3250d916060fff81c8f54e1a125e7d
OM314 <obedmuanda314@gmail.com>
2026-05-30 21:59:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -95,31 +95,31 @@ function processCommand(cmd) {
 
     if (cmd === 'help') {
         reply.innerHTML = `Commandes disponibles :<br>
-        - <span class="highlight">about</span> : Qui suis-je ?<br>
-        - <span class="highlight">projects</span> : Voir mes réalisations<br>
-        - <span class="highlight">clear</span> : Effacer l'écran`;
+        - <span class="highlight">about</span> : En savoir plus sur mon parcours<br>
+        - <span class="highlight">skills</span> : Mes compétences techniques<br>
+        - <span class="highlight">projects</span> : Découvrir mes projets majeurs`;
     } 
     else if (cmd === 'about') {
-        reply.innerHTML = "Je m'appelle Obed, étudiant en informatique. Passionné par les systèmes, les réseaux et le développement.";
+        // Remplis ici avec ton nom et ton domaine d'études !
+        reply.innerHTML = "Je m'appelle Obed Muanda, étudiant en /* À COMPLÉTER */. Passionné par le développement bas niveau et la sécurité.";
+    } 
+    else if (cmd === 'skills') {
+        // Ajoute les langages que tu as appris (C, Python, SQL...)
+        reply.innerHTML = "Technologies maîtrisées : /* À COMPLÉTER */<br>Outils réseau : Cisco Packet Tracer & Huawei eNSP";
     } 
     else if (cmd === 'projects') {
-        reply.innerHTML = `Mes projets majeurs :<br>
-        - <a href="https://github.com/obedd314-sketch/SysWatch" target="_blank" style="color: #00bcff;">SysWatch</a> : Dashboard de supervision réseau et logs (C/Python/SQLite).`;
-    } 
-    else if (cmd === 'clear') {
-        // Optionnel : vide l'écran (on verra comment faire si tu veux l'activer)
-        reply.innerHTML = "Fonctionnalité 'clear' en cours de développement...";
+        // Mets le lien vers ton tout nouveau dépôt SysWatch !
+        reply.innerHTML = `Mon projet principal :<br>
+        - <a href="/* METS TON LIEN GITHUB DE SYSWATCH ICI */" target="_blank" style="color: #00bcff; text-decoration: underline;">SysWatch</a> : Dashboard de supervision de logs (C/Python/Flask/SQLite).`;
     } 
     else if (cmd === '') {
-        return; // Si c'est vide, on ne fait rien
+        return; 
     } 
     else {
-        reply.innerHTML = `Commande inconnue: "${cmd}". Tapez <span class="highlight">help</span> pour obtenir de l'aide.`;
-        reply.style.color = '#ff5f56'; // Texte en rouge pour l'erreur
+        reply.innerHTML = `Commande inconnue: "${cmd}". Tapez <span class="highlight">help</span>.`;
+        reply.style.color = '#ff5f56';
     }
 
     terminalContent.insertBefore(reply, terminalInput.parentElement);
-    
-    // Faire défiler automatiquement le terminal vers le bas
     terminalContent.scrollTop = terminalContent.scrollHeight;
 }
